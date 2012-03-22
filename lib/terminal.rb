2517ba4f517cbfc521056ee13d5efb719b57407c
AriT93 <arit93@yahoo.com>
2012-03-21 21:21:01 -0500

    fixed the regex, twiddle the order for case of price for quantity instead of quantity for price.  translate a word to the number value

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -17,29 +17,31 @@ class Terminal
   def total
     items = @cart.uniq
     retval = 0.00
-    pieces = @cart.count(items[0])
-    product = @products[items[0]]
+    items.each do |item|
+      pieces = @cart.count(item)
+      product = @products[item]
 
-    if pieces > 1 then
-      if product[:deal] then
-        case
-        when pieces == product[:quantity]
-          retval += product[:sale]
-        when  pieces > product[:quantity]
-          if (pieces % product[:quantity]) != 0 then
-            retval += product[:price] * (pieces % product[:quantity])
+      if pieces > 1 then
+        if product[:deal] then
+          case
+          when pieces == product[:quantity]
             retval += product[:sale]
-          elsif  (pieces % product[:quantity].to_i) == 0
-            retval += product[:sale] * (pieces / product[:quantity])
+          when  pieces > product[:quantity]
+            if (pieces % product[:quantity]) != 0 then
+              retval += product[:price] * (pieces % product[:quantity])
+              retval += product[:sale]
+            elsif  (pieces % product[:quantity].to_i) == 0
+              retval += product[:sale] * (pieces / product[:quantity])
+            end
+          when  pieces < product[:quantity].to_i
+            retval +=  product[:price] * pieces
           end
-        when  pieces < product[:quantity].to_i
-           retval +=  product[:price] * pieces
         end
+      else
+        retval +=  product[:price]
       end
-    else
-      retval +=  product[:price]
     end
-     return retval
+    return retval
   end
 
   private
@@ -52,8 +54,18 @@ class Terminal
     if discount
       amount,deal = price.split(' or ')
       amount.gsub!('$','')
-      quantity,sale = deal.match(/(\d+).*for.*\$?(\d+\.\d+)/).to_a[1..2]
+      quantity,sale = deal.match(/(\d+).*for.*\$?(\d+\.\d+|\s\w+\s)/).to_a[1..2]
+    end
+    #do quantity and sale need to flip?
+    if sale.to_f == 0.0
+      quantity,sale = sale,quantity
+      quantity = word_to_number(quantity)
     end
     { :price => amount.to_f, :deal => discount, :quantity => quantity.to_i, :sale => sale.to_f, :string => price}
   end
+  def word_to_number(word)
+    words = %W[zero one two three four five six seven eight nine ten]
+    return words.index(word)
+  end
 end
+#(\d+).*for.*\$?(\d+\.\d+)
